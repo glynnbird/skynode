5c01aeae8c99ab6adade3d327f36c8c864fddbec
Glynn Bird <glynn@scoot.co.uk>
2012-05-21 16:21:10 +0100

    working

--- epg.js
+++ epg.js
@@ -1,5 +1,5 @@
 // a module to store an array of connected users and manage them
-//var sky = require('./includes/sky.js');
+var sky = require('./includes/sky.js');
 var express = require('express')
 var app = express.createServer();
 
@@ -9,15 +9,23 @@ app.set('view options', { pretty: true });
 // output everything in the 'public' directory as static files
 app.use(express.static(__dirname+'/public'));
 
-/* 
-sky.getChannelList( function(epgdata) {
-  console.log(epgdata);
-});
-*/
+// fetch the sky channel list at start up
+//sky.getChannelList();
+
 
 app.get('/', function(req, res){
-    res.render('index.jade', { title: 'My Site',description: 'meta description goes here' });
+  res.render('index.jade', { title: 'My Site',description: 'meta description goes here' });
 });
+app.get('/channels', function(req, res) {
+  sky.getChannelList(function(channelList) {
+    var page = { title: 'Channel List',
+                 description: 'Full channel list',
+                 channels: channelList.channels};
+    res.render('channels.jade', page);
+  })
 
+
+
+});
 
 app.listen(3000);
--- includes/sky.js
+++ includes/sky.js
@@ -2,26 +2,39 @@ var CHANNEL_LIST_URL = "http://epgservices.sky.com/tvlistings-proxy/TVListingsPr
 var http = require('http');
 var Shred = require("shred");
 var shred = new Shred();
+var channelList = false;
 
 var getChannelList = function(callback) {
   
-  
-  var req = shred.get({
-    url: CHANNEL_LIST_URL,
-    headers: {
-      "Accept-Encoding": "gzip"
-    },
-    on: {
-      // You can use response codes as events
-      200: function(response) {
-        callback(response.content.data)
+  if(channelList && callback !=undefined) {
+    console.log("return from cache");
+    callback(channelList)  
+  } else {
+    console.log("fetch from source");
+    var req = shred.get({
+      url: CHANNEL_LIST_URL,
+      headers: {
+        "Accept-Encoding": "gzip"
       },
-      // Any other response means something's wrong
-      response: function(response) {
-        callback(false);
+      on: {
+        // You can use response codes as events
+        200: function(response) {
+          channelList = eval('(' + response.content.body + ')');
+          console.log('done');
+          if(callback != undefined) {
+            callback(channelList)          
+          }
+        },
+        // Any other response means something's wrong
+        response: function(response) {
+          if(callback != undefined) {
+            callback(false);
+          }
+        }
       }
-    }
-  });
+    });
+    
+  }
 }
 
 var getProgrammeList = function(channelFilter,listingDate,callback) {
@@ -48,5 +61,6 @@ var getProgrammeList = function(channelFilter,listingDate,callback) {
 }
 
 module.exports = {
-  getChannelList: getChannelList
+  getChannelList: getChannelList,
+  channelList: channelList
 }
