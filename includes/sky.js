fff709d27b3adad00137f57f2cbc03e327bb9eeb
Glynn Bird <glynn@scoot.co.uk>
2012-05-21 17:41:45 +0100

    added memory cache layer

--- includes/sky.js
+++ includes/sky.js
@@ -3,10 +3,13 @@ var PROGRAMME_LIST_URL = "http://epgservices.sky.com/tvlistings-proxy/TVListings
 var http = require('http');
 var Shred = require("shred");
 var shred = new Shred();
-var channelList = false;
+var cache = require('memory-cache');
+var CHANNEL_LIST_CACHE_KEY='channelList';
+
 
 var getChannelList = function(callback) {
   
+  var channelList = cache.get(CHANNEL_LIST_CACHE_KEY);
   if(channelList && callback !=undefined) {
     console.log("return from cache");
     callback(channelList)  
@@ -21,6 +24,7 @@ var getChannelList = function(callback) {
         // You can use response codes as events
         200: function(response) {
           channelList = eval('(' + response.content.body + ')');
+          cache.put(CHANNEL_LIST_CACHE_KEY,channelList,1000*60*60*12)
           console.log('done');
           if(callback != undefined) {
             callback(channelList)          
@@ -46,27 +50,38 @@ var getProgrammeList = function(channelFilter,listingDate,callback) {
   url=url+"?channels="+escape(channelFilter)+"&dur="+escape(duration)+"&time="+escape(listingDate)+"&detail="+escape(detail);
   console.log(url);
   
-  var req = shred.get({
-    url: url,
-    headers: {
-      "Accept-Encoding": "gzip"
-    },
-      on: {
-        // You can use response codes as events
-        200: function(response) {
-          var programmeData = eval('(' + response.content.body + ')');
-          if(callback != undefined) {
-            callback(programmeData)          
-          }
-        },
-        // Any other response means something's wrong
-        response: function(response) {
-          if(callback != undefined) {
-            callback(false)          
+  
+  // check for a version stored in cache
+  var programmeData = cache.get(url);
+  if(programmeData && callback !=undefined) {
+    console.log("return from cache");
+    callback(programmeData)  
+  } else {
+    console.log("fetch from source");
+  
+    var req = shred.get({
+      url: url,
+      headers: {
+        "Accept-Encoding": "gzip"
+      },
+        on: {
+          // You can use response codes as events
+          200: function(response) {
+            programmeData = eval('(' + response.content.body + ')');
+            cache.put(url,programmeData,1000*60*60*12);
+            if(callback != undefined) {
+              callback(programmeData)          
+            }
+          },
+          // Any other response means something's wrong
+          response: function(response) {
+            if(callback != undefined) {
+              callback(false)          
+            }
           }
         }
-      }
-    });
+      });
+   }
 }
 
 module.exports = {
