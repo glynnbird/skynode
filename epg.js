d85b3a68977d86faebf5084405aef4e7fa61aeb0
Glynn Bird <glynn@scoot.co.uk>
2012-05-22 20:23:21 +0100

    first stab at guide

--- epg.js
+++ epg.js
@@ -14,7 +14,8 @@ app.use(express.static(__dirname+'/public'));
 // home pages
 app.get('/', function(req, res){
   var page = { title: 'Sky Node',
-               description: 'Sky Node - A Sky EPG built in Node.js' }
+               description: 'Sky Node - A Sky EPG built in Node.js',
+               dateList: sky.getValidDateList() }
   res.render('index.jade', page);
 });
 
@@ -23,7 +24,8 @@ app.get('/channels', function(req, res) {
   sky.getChannelList(function(channelList) {
     var page = { title: 'Channel List',
                  description: 'Full channel list',
-                 channels: channelList.channels};
+                 channels: channelList.channels,
+                 dateList: sky.getValidDateList()};
     res.render('channels.jade', page);
   })
 });
@@ -33,10 +35,23 @@ app.get('/channel/:channelno', function(req, res) {
   sky.getProgrammeList(req.params.channelno, sky.getDateString(new Date(),true), function(programmes) {
     var page = { title: 'Channel List - ' + programmes.channels.title,
                  description: 'Full channel list for '+programmes.channels.title,
-                 programmes:  programmes.channels.program};
+                 programmes:  programmes.channels.program,
+                 dateList: sky.getValidDateList()};
     res.render('channel.jade', page);
   })
+});
 
+// get channel guide
+app.get('/guide/:date', function(req, res) {
+  var channelList = "2076,2006,2061,2061,6505,1624,1829,1627";
+  sky.getProgrammeList(channelList, req.params.date, function(data) {
+    console.log(data);
+     var page = { title: 'Guide',
+                  description: 'Programme guide',
+                  channels:  data.channels,
+                  dateList: sky.getValidDateList()};
+     res.render('guide.jade', page);
+   })
 });
 
 app.listen(3000);
--- includes/sky.js
+++ includes/sky.js
@@ -30,6 +30,14 @@ Date.prototype.toSkyDate = function () {
   return year+month.slice(-2)+day.slice(-2)+hour.slice(-2)+minute.slice(-2);
 };
 
+// extend the Date class to export dates in UK's format
+Date.prototype.toUKDate = function () {
+  var year = ""+this.getFullYear();
+  var month = "0" + (this.getMonth()+1);
+  var day = "0" + this.getDate();
+  return year+"/"+month.slice(-2)+"/"+day.slice(-2);
+};
+
 // fetch the full channel list
 var getChannelList = function(callback) {
   
@@ -76,6 +84,7 @@ var getProgrammeList = function(channelFilter,listingDate,callback) {
   var detail=2;
   var duration=24*60; // 1 day
   url=url+"?channels="+escape(channelFilter)+"&dur="+escape(duration)+"&time="+escape(listingDate)+"&detail="+escape(detail);
+  console.log(url);
   
   // check for a version stored in cache
   var programmeData = cache.get(url);
@@ -120,11 +129,41 @@ var getDateString=function(d,atMidnight) {
     d.setMinutes(0);
     d.setSeconds(0);    
   }
-  return d.toSkyDate(d);
+  return d.toSkyDate();
+}
+
+// return a list of dates that the API is good for 
+var getValidDateList=function() {
+  /*
+  [ { ukDate: '2012/05/22', skyDate: '201205220000' },
+    { ukDate: '2012/05/23', skyDate: '201205230000' },
+    { ukDate: '2012/05/24', skyDate: '201205240000' },
+    { ukDate: '2012/05/25', skyDate: '201205250000' },
+    { ukDate: '2012/05/26', skyDate: '201205260000' },
+    { ukDate: '2012/05/27', skyDate: '201205270000' },
+    { ukDate: '2012/05/28', skyDate: '201205280000' } ]
+  */
+  var d = new Date();
+  d.setHours(0);
+  d.setMinutes(0);
+  d.setSeconds(0);
+  var i=0;
+  var dateList = new Array();
+  do {
+    var ob = new Object();
+    ob.ukDate = d.toUKDate();
+    ob.skyDate = d.toSkyDate();
+    dateList[i] = ob;
+    // move forward a day
+    d = new Date(d.getTime() + 1000 * 60 * 60 * 24);
+    i++;
+  } while(i<7);
+  return dateList;
 }
 
 module.exports = {
   getChannelList: getChannelList,
   getProgrammeList: getProgrammeList,
-  getDateString: getDateString
+  getDateString: getDateString,
+  getValidDateList: getValidDateList
 }
